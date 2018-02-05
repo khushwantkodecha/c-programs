dadccfcf60cf25bc24d11739963093ca73cdd622
khushwantkodecha <mail2mrroot@gmail.com>
2018-02-06 00:07:00 +0545

    added

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a;
+ int b;
+ printf("enter the value of a and b\n");
+ scanf("%d%d",&a,&b);
+ printf("sum of a and b is %d",a+b);
+ getch();
+}
